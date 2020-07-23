e846069fd1ced69e52c0b055ce49175b0fbb6667
mayurnovitekk <mayur.novitekk@gmail.com>
2020-07-23 21:47:04 +0530

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
 
-
 var app = express();
 
 app.use(bodyParser.json());
@@ -28,7 +27,7 @@ app.listen(3000, () => {
   console.log('Started port 3000');
 });
 
-
+module.exports = {app};
 
 
 
